221075c7115ac8edefdc766f88676f573f25491f
ConsaniEdu <consani.eduardo@gmail.com>
2024-03-07 17:32:04 -0300

    fiz tuto

--- js/autenticação.js
+++ js/autenticação.js
@@ -1,1 +1,15 @@
-const usuario
+const usuario = document.getElementById("usuario");
+const senha = document.getElementById("senha");
+
+document.getElementsByClassName("formLogin")[0].addEventListener("submit", function (e){
+   e.preventDefault(); 
+
+if (usuario.ariaValueMax.trim()=="" || senha.value.trim()==""){
+    alert("preencha os campos");
+}
+
+else{
+    alert("login bem-sucedido");
+    window.location.href="file:///C:/Users/Senai/Documents/Consani-projeto/pages/loja.html"
+}
+});
